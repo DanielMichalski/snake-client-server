7ab47593486f83fdd5af113e2f64b15ea7d802e8
bakeraw2 <notarget1>
2014-05-06 10:01:58 +0200

    Menu glowne

---
+++ src/main/java/pl/bestsoft/snake/util/Const.java
@@ -0,0 +1,10 @@
+package pl.bestsoft.snake.util;
+
+import java.awt.*;
+
+public class Const {
+    public static interface Colors {
+        Color BACKGROUND_COLOR = new Color(43, 43, 43);
+        Color LABEL_COLOR = new Color(143, 145, 143);
+    }
+}
